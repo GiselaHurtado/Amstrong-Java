f3308c142b6d4b1fcd34d8acda01d57f52e185f4
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-07-20 12:08:36 +0200

    test done

--- src/test/java/dev/gisela/AmstrongTest.java
+++ src/test/java/dev/gisela/AmstrongTest.java
@@ -7,15 +7,17 @@ import org.junit.jupiter.api.Test;
 
 public class AmstrongTest {
     @Test
-    void testMain() {
-           assertTrue(Amstrong.isAmstrong(0));  
+    public void isAmstrong() {
+        assertTrue(Amstrong.isAmstrong(0));  
         assertTrue(Amstrong.isAmstrong(1));  
         assertTrue(Amstrong.isAmstrong(153)); 
         assertTrue(Amstrong.isAmstrong(370)); 
         assertTrue(Amstrong.isAmstrong(371)); 
         assertTrue(Amstrong.isAmstrong(407)); 
-
-      
+    }
+    
+        @Test
+        public void isNotAmstrong() {
         assertFalse(Amstrong.isAmstrong(10));  
         assertFalse(Amstrong.isAmstrong(100)); 
         assertFalse(Amstrong.isAmstrong(123)); 
